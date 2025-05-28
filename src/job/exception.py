d877c82467ee964f4ee4009463fcd6e8682f52cc
Jeremy Shen <jeremy.shen@lileesystems.com>
2025-05-28 22:27:37 +0800

    Remove Need not information

--- src/job/exception.py
+++ src/job/exception.py
@@ -1,6 +1,4 @@
 class NotFoundException(Exception):
-    """Raised when an entity is not found in the database."""
-
     def __init__(self, message: str):
         super().__init__(message)
         self.message = message
